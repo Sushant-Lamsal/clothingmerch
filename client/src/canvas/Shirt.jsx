615ae6331050c8be559f37e84ae56bf19828e1c5
Sushant Lamsal <sushantlamsal31@gmail.com>
2023-08-03 20:15:00 +0545

    ggwp

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -7,48 +7,48 @@ import { Decal, useGLTF, useTexture } from '@react-three/drei';
 import state from '../store';
 
 const Shirt = () => {
-  const snap = useSnapshot(state);
-  const { nodes, materials } = useGLTF('/shirt_baked.glb');
+const snap = useSnapshot(state);
+const { nodes, materials } = useGLTF('/shirt_baked.glb');
 
-  const logoTexture = useTexture(snap.logoDecal);
-  const fullTexture = useTexture(snap.fullDecal);
+const logoTexture = useTexture(snap.logoDecal);
+const fullTexture = useTexture(snap.fullDecal);
 
-  useFrame((state, delta) => easing.dampC(materials.lambert1.color, snap.color, 0.25, delta));
+useFrame((state, delta) => easing.dampC(materials.lambert1.color, snap.color, 0.25, delta));
 
-  const stateString = JSON.stringify(snap);
+const stateString = JSON.stringify(snap);
 
-  return (
+return (
     <group key={stateString}>
-      <mesh
+    <mesh
         castShadow
         geometry={nodes.T_Shirt_male.geometry}
         material={materials.lambert1}
         material-roughness={1}
         dispose={null}
-      >
+    >
         {snap.isFullTexture && (
-          <Decal 
+        <Decal 
             position={[0, 0, 0]}
             rotation={[0, 0, 0]}
             scale={1}
             map={fullTexture}
-          />
+        />
         )}
 
         {snap.isLogoTexture && (
-          <Decal 
+        <Decal 
             position={[0, 0.04, 0.15]}
             rotation={[0, 0, 0]}
             scale={0.15}
             map={logoTexture}
-            // map-anisotropy={16}
+            // map-anisotropy={2}
             depthTest={false}
             depthWrite={true}
-          />
+        />
         )}
-      </mesh>
+    </mesh>
     </group>
-  )
+)
 }
 
 export default Shirt
