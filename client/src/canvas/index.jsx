269a133a3c399d1fca59fc70cbfe4487c01a031a
Sushant Lamsal <sushantlamsal31@gmail.com>
2023-08-03 18:47:59 +0545

    shit rendering

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -6,12 +6,20 @@ import Backdrop from './Backdrop'
 import CameraRig from './CameraRig'
 
 
-const Canvas = () => {
+const CanvasModel = () => {
 return (
     <Canvas>
         <ambientLight intensity={0.5}/>
+        <Environment preset="city" />
+
+        <CameraRig>
+            {/* <Backdrop/> */}
+            <Center>
+                <Shirt/>
+            </Center>
+        </CameraRig>
     </Canvas>
     )
 }
 
-export default Canvas ;
+export default CanvasModel ;
