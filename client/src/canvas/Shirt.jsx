c436d2bd3ec30f77d3c2c77ffd89d4eb0066cf91
Sushant Lamsal <sushantlamsal31@gmail.com>
2023-08-05 20:09:47 +0545

    ai not working

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -41,7 +41,7 @@ return (
             rotation={[0, 0, 0]}
             scale={0.15}
             map={logoTexture}
-            // map-anisotropy={2}
+            // map-anisotropy={16}
             depthTest={false}
             depthWrite={true}
         />
